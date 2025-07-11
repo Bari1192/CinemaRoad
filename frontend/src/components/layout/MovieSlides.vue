919c2bd9249ff0571bc19d756a08144117122756
bari1192 <barabas.m4rk@gmail.com>
2025-07-11 14:03:13 +0000

    As a result of the consultation MoovieSlides second modifications has been accepted.

--- frontend/src/components/layout/MovieSlides.vue
+++ frontend/src/components/layout/MovieSlides.vue
@@ -1,42 +1,22 @@
 <template>
-    <div class="banner_slider mx-auto min-w-full min-h-72 bg-slate-700/15 py-8 ">
+    <div class="banner_slider mx-auto min-w-full h-full bg-slate-700/15 py-8 ">
         <p class="uppercase py-4 text-5xl text-white font-extrabold tracking-wider text-center mb-4">
-            Új a kínálatban
+            Helyszíneink - Városszerte
         </p>
-        <div class="grid grid-cols-6 w-full h-full items-center space-x-2 mx-4 ">
-
+        <div class="container h-full grid grid-cols-4 justify-center min-h-[50dvh] items-center space-x-8 mx-auto ">
             <div
-                class="card relative gap-2 flex justify-center items-center text-pink-300 bg-slate-600 w-full max-w-[320px] min-h-[16rem] border-2 border-rose-100 text-2xl font-semibold">
+                class="card rotate-90 gap-2 flex text-pink-300 bg-slate-600  max-w-[320px] min-h-[20rem] border-2 border-rose-100 text-2xl font-semibold overflow-hidden">
                 <img src="https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421"
                     class="h-[450px] w-full max-w-[350px]" alt="">
                 <span
                     class="horror-label absolute bg-red-600 shadow-lg text-lg tracking-wider font-semibold py-2 px-3 text-white right-0 top-0 overflow-hidden rounded-bl-lg rounded-tr-lg">
                     Horror
                 </span>
+                
             </div>
 
             <div
-                class="card relative gap-2 flex justify-center items-center text-pink-300 bg-slate-600 w-full max-w-[320px] min-h-[16rem] border-2 border-rose-100 text-2xl font-semibold">
-                <img src="https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421"
-                    class="h-[450px] w-full" alt="">
-                <span
-                    class="horror-label absolute bg-yellow-600 shadow-lg text-lg tracking-wider font-semibold py-2 px-3 text-white right-0 top-0 overflow-hidden rounded-bl-lg rounded-tr-lg">
-                    Vígjáték
-                </span>
-            </div>
-
-            <div
-                class="card relative gap-2 flex justify-center items-center text-pink-300 bg-slate-600 w-full max-w-[320px] min-h-[16rem] border-2 border-rose-100 text-2xl font-semibold">
-                <img src="https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421"
-                    class="h-[450px] w-full" alt="">
-                <span
-                    class="horror-label absolute bg-sky-600 shadow-lg text-lg tracking-wider font-semibold py-2 px-3 text-white right-0 top-0 overflow-hidden rounded-bl-lg rounded-tr-lg">
-                    Dráma
-                </span>
-            </div>
-
-            <div
-                class="card relative gap-2 flex justify-center items-center text-pink-300 bg-slate-600 w-full max-w-[320px] min-h-[16rem] border-2 border-rose-100 text-2xl font-semibold">
+                class="card gap-2 flex text-pink-300 bg-slate-600 w-full max-w-[320px] min-h-[20rem] border-2 border-rose-100 text-2xl font-semibold overflow-hidden">
                 <img src="https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421"
                     class="h-[450px] w-full" alt="">
                 <span
@@ -46,7 +26,7 @@
             </div>
 
             <div
-                class="card relative gap-2 flex justify-center items-center text-pink-300 bg-slate-600 w-full max-w-[320px] min-h-[16rem] border-2 border-rose-100 text-2xl font-semibold">
+                class="card gap-2 flex text-pink-300 bg-slate-600 w-full max-w-[320px] min-h-[20rem] border-2 border-rose-100 text-2xl font-semibold overflow-hidden">
                 <img src="https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421"
                     class="h-[450px] w-full" alt="">
                 <span
@@ -56,7 +36,7 @@
             </div>
 
             <div
-                class="card relative gap-2 flex justify-center items-center text-pink-300 bg-slate-600 w-full max-w-[320px] min-h-[16rem] border-2 border-rose-100 text-2xl font-semibold">
+                class="card gap-2 flex text-pink-300 bg-slate-600 w-full max-w-[320px] min-h-[20rem] border-2 border-rose-100 text-2xl font-semibold overflow-hidden">
                 <img src="https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421"
                     class="h-[450px] w-full" alt="">
                 <span

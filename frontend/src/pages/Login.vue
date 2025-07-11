9eadb02a188afa79f197a0b8fefbcafe75c25bc9
NemethTamas1 <nemethtamas029@gmail.com>
2025-07-11 17:01:15 +0000

    Login site mobile view fixed, validation added

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -2,28 +2,38 @@
     <BaseLayout>
         <h1 class="text-5xl font-semibold text-center py-10">Bejelentkezés</h1>
 
-        <FormKit type="form" :actions="false">
-            <div class="w-8/12 mx-auto border border-pink-600 p-6 rounded-lg">
+        <div class="w-8/12 mx-auto border border-pink-600 p-6 rounded-lg">
 
-                <div class="w-6/12 mx-auto mb-4">
-                    <FormKit type="email" name="email" label="E-mail" label-class="text-xl"
-                    input-class="rounded-lg p-2 w-full" />
+            <FormKit type="form" :actions="false">
+
+                <div class="w-full md:w-1/2 mx-auto">
+                    <FormKit
+                        type="email"
+                        name="email"
+                        label="E-mail"
+                        label-class="text-xl"
+                        input-class="rounded-lg p-2 w-full"
+                        validation="email"
+                        />
                 </div>
 
-                <div class="w-6/12 mx-auto mb-4">
+                <div class="w-full md:w-1/2 mx-auto">
                     <FormKit type="password" name="password" label="Jelszó" label-class="text-xl"
                     input-class="rounded-lg p-2 w-full" />
 
+                    <RouterLink :to="'/Registration'">
+                        Még nem regisztrált? Kattintson ide!
+                    </RouterLink>
                 </div>
 
                 <div class="text-center">
-                    <button
-                        class="bg-gray-800 text-pink-600 py-3 px-6 rounded-lg text-xl font-semibold w-4/12 md:w-1/3">Bejelentkezés</button>
+                    <button class="bg-gray-800 text-pink-600 py-3 mt-4 px-6 rounded-lg text-xl font-semibold w-full md:w-auto">Bejelentkezés</button>
                 </div>
 
+            </FormKit>
 
-            </div>
-        </FormKit>
+        </div>
+
     </BaseLayout>
 </template>
 

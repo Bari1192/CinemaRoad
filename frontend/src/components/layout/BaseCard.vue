6d4c031dd40ea39c4e874dfe7e0fa4addd818d89
bari1192 <barabas.m4rk@gmail.com>
2025-07-14 19:06:21 +0000

    BaseCard layout template has been created for dinamyc uses.

---
+++ frontend/src/components/layout/BaseCard.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+
+<template>
+  <div class="card w-full h-full bg-slate-900 rounded-2xl shadow-xl overflow-hidden border border-slate-700 min-h-[24rem] transition-all duration-200 hover:scale-105
+        hover:cursor-pointer">
+    <div class="relative">
+      <img :src="props.src" :alt="props.alt" class="w-full h-full object-cover" />
+      <span
+        class="absolute right-0 top-0 text-xs md:text-base font-bold md:font-medium tracking-widest py-1 md:py-2 px-4 md:px-6 rounded-bl-xl rounded-tr-2xl shadow-md shadow-gray-800 uppercase bg-gray-200">
+        {{ props.type }}
+      </span>
+    </div>
+    <div class="flex-1 flex flex-col justify-between px-4 py-3">
+      <h3 class="text-lg font-semibold text-white mb-1 truncate">{{ props.title }}</h3>
+      <p class="text-sm text-slate-200 mb-3 line-clamp-3">{{ props.description }}</p>
+      <button
+        class="mt-auto w-full bg-pink-700 hover:bg-pink-600 text-white font-semibold rounded-xl py-2 transition-all">
+        Megnézem
+      </button>
+    </div>
+  </div>
+</template>
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,12 +1,41 @@
+<script setup>
+import BaseHeader from "@components/layout/BaseHeader.vue";
+import MovieThemes from "@components/layout/MovieThemes.vue";
+import ClickBanner from "@components/layout/ClickBanner.vue";
+import BaseFooter from "@components/layout/BaseFooter.vue";
+import BaseCard from "@components/layout/BaseCard.vue";
+</script>
+
+
 <template>
   <BaseHeader />
   <div class="mx-auto w-full min-h-screen">
 
     <ClickBanner />
 
-    <MovieSlides />
-
     <div class="container mx-auto w-full h-full">
+
+      <div class="mx-auto w-full h-full bg-slate-950 py-8 flex justify-center">
+        <div class="w-full md:lg:max-w-5xl lg:max-w-[1920px] grid grid-cols-1 gap-12 px-10 sm:grid-cols-2 lg:gap-6 lg:grid-cols-4">
+          <BaseCard :title="'Autós üldözés'" :type="'Akció'"
+            :description="'Lélegzetelállító kocsis üldözések, menekülés és adrenalin—csak a legbátrabbaknak.'"
+            :src="'https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421'"
+            :alt="'Autós üldözés'" />
+          <BaseCard :title="'Autós üldözés'" :type="'Akció'"
+            :description="'Lélegzetelállító kocsis üldözések, menekülés és adrenalin—csak a legbátrabbaknak.'"
+            :src="'https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421'"
+            :alt="'Autós üldözés'" />
+          <BaseCard :title="'Autós üldözés'" :type="'Akció'"
+            :description="'Lélegzetelállító kocsis üldözések, menekülés és adrenalin—csak a legbátrabbaknak.'"
+            :src="'https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421'"
+            :alt="'Autós üldözés'" />
+          <BaseCard :title="'Autós üldözés'" :type="'Akció'"
+            :description="'Lélegzetelállító kocsis üldözések, menekülés és adrenalin—csak a legbátrabbaknak.'"
+            :src="'https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421'"
+            :alt="'Autós üldözés'" />
+        </div>
+      </div>
+
       <h1 class="text-6xl my-10 text-white">Welcome banner</h1>
 
       <div class="container mx-auto w-full my-12">
@@ -16,8 +45,9 @@
 
             <div
               class="w-full h-full text-center items-center min-h-[400px] flex justify-center mx-auto font-extrabold text-3xl text-white bg-slate-500">
-              [Jobb kép] AI kép autóból kitekintés Mozivászon irányába. Popcorn evés, férfi + nő. nézőpont a hátuk mögül.</div>
-              [Bal szöveg] "Retro élmény, modern környezet"
+              [Jobb kép] AI kép autóból kitekintés Mozivászon irányába. Popcorn evés, férfi + nő. nézőpont a hátuk
+              mögül.</div>
+            [Bal szöveg] "Retro élmény, modern környezet"
             <div
               class="w-full text-center items-center mx-auto min-h-[400px] flex justify-center font-extrabold text-3xl text-white bg-pink-800">
               <img
@@ -39,16 +69,9 @@
   <BaseFooter />
 </template>
 
-<script setup>
-import BaseHeader from "@components/layout/BaseHeader.vue";
-import MovieThemes from "@components/layout/MovieThemes.vue";
-import ClickBanner from "@components/layout/ClickBanner.vue";
-import MovieSlides from "@components/layout/MovieSlides.vue";
-import BaseFooter from "@components/layout/BaseFooter.vue";
-</script>
+
 
 <style>
-
 .card {
   position: relative;
   border-radius: 12px;

778d0133ff6d14573e37110eacf1cb23ecdd0e8e
bari1192 <barabas.m4rk@gmail.com>
2025-07-15 12:01:46 +0000

    final modifications in responsivity with a BaseCard. Provided responsive design in sm/md/lg/xl device sizes.

--- frontend/src/components/layout/BaseCard.vue
+++ frontend/src/components/layout/BaseCard.vue
@@ -26,21 +26,23 @@ const props = defineProps({
 
 
 <template>
-  <div class="card w-full h-full bg-slate-900 rounded-2xl shadow-xl overflow-hidden border border-slate-700 min-h-[24rem] transition-all duration-200 hover:scale-105
+  <div class="card w-full h-full bg-slate-900 shadow-xl overflow-hidden border border-slate-700 transition-all duration-200 hover:scale-105
         hover:cursor-pointer">
     <div class="relative">
       <img :src="props.src" :alt="props.alt" class="w-full h-full object-cover" />
       <span v-if="props.type && props.type=='action'"
-        class="text-sky-700 absolute right-0 top-0 text-xs md:text-base font-bold tracking-widest py-1 md:py-2 px-4 md:px-6 rounded-bl-xl rounded-tr-2xl shadow-md shadow-gray-800 uppercase bg-gray-200">
+        class="text-sky-700 absolute right-0 top-0 text-sm md:text-base font-bold tracking-widest rounded-bl-xl rounded-tr-xl shadow-md shadow-gray-800 uppercase bg-gray-200 py-2.5 px-6
+        md:px-6 md:py-2 lg:px-4 lg:py-1.5">
         Akció
       </span>
       <span v-if="props.type && props.type=='family'"
-        class="text-yellow-700 absolute right-0 top-0 text-xs md:text-base font-bold tracking-widest py-1 md:py-2 px-4 md:px-6 rounded-bl-xl rounded-tr-2xl shadow-md shadow-gray-800 uppercase bg-gray-200">
+        class="text-yellow-700 absolute right-0 top-0 text-sm md:text-base font-bold tracking-widest rounded-bl-xl rounded-tr-xl shadow-md shadow-gray-800 uppercase bg-gray-200 py-2.5 px-6
+        md:px-6 md:py-2 lg:px-4 lg:py-1.5">
         Családi
       </span>
       <span v-if="props.type && props.type=='horror'"
-        class="absolute right-0 top-0 text-xs md:text-base font-bold tracking-widest py-1 md:py-2 px-4 md:px-6 rounded-bl-xl rounded-tr-2xl shadow-md shadow-gray-800 uppercase bg-gray-200
-        text-red-700">
+        class="text-red-700 absolute right-0 top-0 text-sm md:text-base font-bold tracking-widest rounded-bl-xl rounded-tr-xl shadow-md shadow-gray-800 uppercase bg-gray-200 py-2.5 px-6
+        md:px-6 md:py-2 lg:px-4 lg:py-1.5">
         Horror
       </span>
     </div>
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -16,8 +16,7 @@ import BaseCard from "@components/layout/BaseCard.vue";
     <div class="container mx-auto w-full h-full">
 
       <div
-        class="card mx-auto w-full lg:w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-center gap-6 lg:gap-12">
-
+        class="card mx-auto px-6 lg:px-0 w-full lg:w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-center gap-6 md:gap-8 lg:gap-6 xl:gap-10">
         <BaseCard :title="'Autós üldözés'" :type="'action'"
           :description="'Lélegzetelállító kocsis üldözések, menekülés és adrenalin—csak a legbátrabbaknak.'"
           :src="'https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421'"

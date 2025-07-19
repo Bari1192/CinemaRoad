eabe87b5d8d7e0f8507575d99988d63b45fe2d28
NemethTamas1 <nemethtamas029@gmail.com>
2025-07-19 18:49:44 +0000

    Location now has the movie cards, and also non working filter buttons

--- frontend/src/components/[locationName]Details.vue
+++ frontend/src/components/[locationName]Details.vue
@@ -16,6 +16,36 @@
                 </p>
             </div>
         </div>
+
+        <h2 class="text-3xl font-semibold pt-14">Aktuális filmkínálatunk a {{ cinema.name }} helyszínen:</h2>
+
+        <div class="flex gap-3">
+            <button
+                class="p-2 rounded-lg"
+                :class="selectedGenre === 'Mind' ? 'bg-pink-600 text-white' : 'bg-white text-600'"
+                @click="selectedGenre='Mind'">Mind</button>
+
+            <button
+                class="p-2 rounded-lg"
+                :class="selectedGenre === 'Horror' ? 'bg-pink-600 text-white' : 'bg-white text-600'"
+                @click="selectedGenre='Horror'">Horror</button>
+
+            <button
+                class="p-2 rounded-lg"
+                :class="selectedGenre === 'Romantikus' ? 'bg-pink-600 text-white' : 'bg-white text-600'"
+                @click="selectedGenre='Romantikus'">Romantikus</button>
+        </div>
+        <div v-if="screenings.length > 0" class="grid grid-cols-1 md:grid-cols-5 gap-6 mt-10">
+            <BaseCard
+                v-for="screening in enrichedScreenings"
+                :key="screening.id"
+                :title="screening.movie?.title"
+                :type="screening.movie?.genre"
+                :src="`https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/d6/20249e4b290423cd14d4fbb932faa88a_500x735.jpg?t=1725823421`"
+                :alt="screening.movie?.title"
+                :description="screening.description"
+                />
+        </div>
     </div>
 
     <div v-else>
@@ -27,12 +57,17 @@
 <script setup>
 import { useDriveInCinemaStore } from '@stores/DriveInCinemaStore.js';
 import BaseSpinner from '@components/layout/BaseSpinner.vue';
+import BaseCard from '@components/BaseCard.vue';
 
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 import { useRoute } from 'vue-router';
+import { http } from '@utils/http.mjs';
 
 const cinema = ref(null);
 const route = useRoute();
+const screenings = ref([]);
+const movies = ref([]);
+const selectedGenre = ref('');
 
 function slugify(text) {
     return text
@@ -45,7 +80,7 @@ function slugify(text) {
     .replace(/\s+/g, '-')
     .replace(/[^\w\-]+/g, '')
     .replace(/\-\-+/g, '-')
-    .replace(/^-+/, '')
+    .replace(/^-+/, '') 
     .replace(/-+$/, '');
 }
 
@@ -56,6 +91,28 @@ onMounted( async() => {
     cinema.value = driveInCinemaStore.driveInCinemas.find(c =>
         slugify(c.name) === route.params.locationName.toLowerCase()
     );
+
+    if(cinema.value) {
+        console.log("cinema ID: ", cinema.value.id)
+
+        const response = await http.get("/screenings");
+        screenings.value = response.data.data.filter(
+            screening => screening.drive_in_cinema_id === cinema.value.id
+        );
+
+        const moviesResponse = await http.get('/movies');
+        movies.value = moviesResponse.data.data;
+    }
+});
+
+const enrichedScreenings = computed(() => {
+  return screenings.value.map(screening => {
+    const movie = movies.value.find(m => m.id === screening.movie_id);
+    return {
+      ...screening,
+      movie: movie || null
+    };
+  });
 });
 
 </script>

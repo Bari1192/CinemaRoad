8df765b1898aa05f14cd28858e875064ba4fb895
bari1192 <barabas.m4rk@gmail.com>
2025-07-31 12:10:14 +0000

    mail system frame has been created. After merge to dev should be connected with a real data system/flow.

---
+++ backend/app/Observers/ReservationObserver.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Observers;
+
+use App\Mail\ReservationConfirmation;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\Mail;
+
+class ReservationObserver
+{
+    /**
+     * Handle the Reservation "created" event.
+     */
+    public function created(Reservation $reservation): void
+    {
+        if ($reservation->user && $reservation->user->email) {
+            Mail::to($reservation->user->email)->send(new ReservationConfirmation($reservation));
+        }
+    }
+
+    /**
+     * Handle the Reservation "updated" event.
+     */
+    public function updated(Reservation $reservation): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Reservation "deleted" event.
+     */
+    public function deleted(Reservation $reservation): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Reservation "restored" event.
+     */
+    public function restored(Reservation $reservation): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Reservation "force deleted" event.
+     */
+    public function forceDeleted(Reservation $reservation): void
+    {
+        //
+    }
+}

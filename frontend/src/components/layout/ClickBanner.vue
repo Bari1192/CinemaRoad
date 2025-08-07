8bee5dcdeb7a19d71fcde5bf305ab49cddf328db
bari1192 <barabas.m4rk@gmail.com>
2025-08-07 15:05:21 +0000

    Clickbanner images have been updated with generated images. Responsivity has been improved in phone and tablet also.

--- frontend/src/components/layout/ClickBanner.vue
+++ frontend/src/components/layout/ClickBanner.vue
@@ -2,15 +2,18 @@
 
 import { ref, onMounted, onUnmounted, computed } from 'vue';
 
+import dustzoneImg from '@/assets/img/Main_Slider_img/Dustzone_last_run.webp'
+import nightfireImg from '@/assets/img/Main_Slider_img/Nightfire_redemption.webp'
+import realmshatterImg from '@/assets/img/Main_Slider_img/Realmshatter.webp'
 const movies = ref([
-  { imageUrl: 'https://ocdn.eu/images/pulscms/ZjE7MDA_/f875191280dab9906f44e247ebae3dca.jpeg', title: 'A galaxis őrzői vol. 3' },
-  { imageUrl: 'https://sm.ign.com/ign_hu/screenshot/default/dune-2021-1200-1200-675-675-crop-000000_rqk4.jpg', title: 'Dűne' },
-  { imageUrl: 'https://staticeu.sweet.tv/images/cache/v2/movie_banner/CPi6ARICZW4aAkhVIAIoFjAm/23928-star-wars-the-force-awakens_1280x720.jpg', title: 'Star Wars: Az ébredő Erő' },
-]);
+  { imageUrl: dustzoneImg, title: 'Dustzone: Last Run' },
+  { imageUrl: nightfireImg, title: 'Nightfire: Redemption' },
+  { imageUrl: realmshatterImg, title: 'Realmshatter' },
+])
 
 const activeIndex = ref(0);
 let autoplayInterval = null;
-const autoplayDelay = 4000;
+const autoplayDelay = 6000;
 let direction = 1;
 
 const isPrevDisabled = computed(() => activeIndex.value === 0);
@@ -40,7 +43,7 @@ const goToSlide = (index) => {
   if (activeIndex.value === movies.value.length - 1) direction = -1;
   else if (activeIndex.value === 0) direction = 1;
   stopAutoplay();
-  
+
   setTimeout(() => {
     startAutoplay()
   }, 8000);
@@ -76,19 +79,20 @@ onUnmounted(() => {
     <div class="carousel-container h-full w-full min-h-[40dvh] max-h-[70dvh] xl:h-[90vh]" @mouseenter="stopAutoplay"
       @mouseleave="startAutoplay">
       <div v-for="(movie, index) in movies" :key="index" :class="[
-        'carousel-item absolute w-full h-full transition-transform duration-1000 ease-in-out overflow-hidden border-y-4 border-slate-600 rounded-2xl',
+        'carousel-item absolute w-full h-full transition-transform duration-1000 ease-in-out overflow-hidden border-y-4 border-slate-600 lg:rounded-2xl',
         { 'active-slide': index === activeIndex }
       ]" :style="getSlideTransform(index)">
         <img :src="movie.imageUrl" :alt="movie.title"
-          class="w-full h-full object-cover rounded-lg shadow-xl overflow-hidden">
-        <div class="absolute right-0 top-0 bg-gradient-to-b from-black via-black/70 to-transparent text-white rounded-r-lg overflow-hidden
+          class="w-full h-full object-cover lg:rounded-lg shadow-xl overflow-hidden">
+        <div class="absolute right-0 top-0 bg-gradient-to-b from-black via-black/70 to-transparent text-white lg:rounded-r-lg overflow-hidden
         pl-6 pt-2 pr-4 pb-8
         md:pl-8 md:pt-3 md:pb-12
         lg:pl-12 lg:pt-8 lg:pb-16
         xl:pl-12 xl:pt-8 xl:pb-16
         ">
           <h3 class="text-base md:text-xl lg:text-2xl xl:text-4xl font-bold mt-1 text-right">{{ movie.title }}</h3>
-          <p class="text-sm md:text-lg lg:text-xl xl:text-2xl text-right italic">Premier előtti vetítés!</p>
+          <p class="before_premier text-sm md:text-lg lg:text-xl xl:text-2xl text-right italic lg:mt-1.5 text-pink-500"
+            :class="movie.title === 'Dustzone: Last Run' ? 'text-pink-200 font-semibold' : ''">Premier előtti vetítés</p>
         </div>
       </div>
     </div>
@@ -122,9 +126,10 @@ onUnmounted(() => {
     </button>
 
     <div
-      class="lg:hidden carousel-indicators absolute bottom-2 left-1/2 -translate-x-1/2 flex space-x-2 z-10 bg-gray-400/70 rounded-lg p-1">
+      class="lg:hidden mb-2 carousel-indicators absolute bottom-2 left-1/2 -translate-x-1/2 flex space-x-2 z-10 bg-gray-400/70 rounded-lg p-1">
       <button v-for="(movie, index) in movies" :key="'indicator-' + index" @click="goToSlide(index)"
-        :class="['w-4 h-4 rounded-full bg-gray-400 border border-white/25 shadow-sm shadow-black/50 hover:bg-pink-100 transition-colors duration-300', { 'bg-pink-500': index === activeIndex }]"></button>
+        :class="['md:w-6 md:h-6 w-4 h-4 rounded-full bg-gray-400 border border-white/25 shadow-sm shadow-black/50 hover:bg-pink-100 transition-colors duration-300'
+        , { 'bg-pink-500': index === activeIndex }]"></button>
     </div>
 
   </div>
@@ -141,4 +146,12 @@ onUnmounted(() => {
   opacity: 1;
   z-index: 5;
 }
+
+.before_premier {
+  font-family: 'Noto Sans', 'Times New Roman', Times, serif;
+  font-style: unset;
+  text-shadow: 0 0 25px rgb(158, 119, 144);
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Cookie&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Onest:wght@100..900&display=swap');
 </style>

68b9cd15c806bb2d730d6509280ef8482df1cf2c
NemethTamas1 <nemethtamas029@gmail.com>
2025-08-15 12:05:24 +0000

    Summary page now can manage user purchase and reservation, and guest's purchase

--- frontend/src/pages/Summary.vue
+++ frontend/src/pages/Summary.vue
@@ -1,22 +1,26 @@
 <script setup>
 import BaseLayout from '@layouts/BaseLayout.vue'
 import Stepper from '@components/layout/Stepper.vue'
+import BaseSpinner from '@components/layout/BaseSpinner.vue'
 import { useTicketStore } from '@stores/TicketStore'
 import { useUserStore } from '@stores/UserStore'
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const userStore = useUserStore();
 const ticketStore = useTicketStore();
-
 const router = useRouter();
 
 const locationName = computed(() =>
     ticketStore.location?.name || ticketStore.locationName || '–'
 )
-const locationDesc = computed(() =>
-    ticketStore.location?.description || ''
-)
+
+// const locationDesc = computed(() =>
+//     ticketStore.location?.description || ''
+// )
+
+const bookingType = ref(null);
+const guestEmail = ref(null);
 
 const movieTitle = computed(() =>
     ticketStore.movie?.title || (typeof ticketStore.movie === 'string' ? ticketStore.movie : '–')
@@ -45,99 +49,162 @@ const formattedTime = computed(() => {
     return `${dateObj.toLocaleDateString('hu-HU', { year: 'numeric', month: '2-digit', day: '2-digit' })} (${dateObj.toLocaleDateString('hu-HU', { weekday: "long" })}) ${dateObj.toLocaleTimeString('hu-HU', { hour: '2-digit', minute: '2-digit' })}`
 });
 
+
+
+
 const handleConfirmation = async () => {
+    if (!bookingType.value && userStore.userID) {
+        alert("Kérlek válassz! Foglalni, vagy vásárolni szeretnél?");
+        return;
+    }
+
     const parkingSpots = Array.isArray(ticketStore.parkingSpot)
         ? ticketStore.parkingSpot
         : [ticketStore.parkingSpot];
 
-        const parkingSpotsConcat = parkingSpots.join(',');
-    const reservationData = {
-        user_id: userStore.userID,
-        screening_id: ticketStore.time.id,
-        location_id: ticketStore.location.id,
-        reserved_at: ticketStore.time.start_time,
-        parkingspot: parkingSpotsConcat
-    };
+    // HA BE VAN JELENTKEZVE, ÉS FOGLAL
+    if (bookingType.value === 'reservation' && userStore.userID) {
+        const payload = {
+            user_id: userStore.userID,
+            screening_id: ticketStore.time.id,
+            location_id: ticketStore.location.id,
+            reserved_at: ticketStore.time.start_time,
+            parkingspot: parkingSpots.join(',')
+        };
+        console.log("Reservation bejelentkezett payload: ", payload);
+        await ticketStore.postTicketReservation(payload);
+    }
+    //HA BE VAN JELENTKEZVE ÉS VESZI
+    else if ((bookingType.value === 'purchase' && userStore.userID)) {
+        const payload = {
+            location_id: ticketStore.location.id,
+            movie_id: ticketStore.movie.id,
+            screening_id: ticketStore.time.id,
+            parkingspot: parkingSpots.join(','),
+            user_id: userStore.userID
+        };
+        console.log("Purchase bejelentkezett payload: ", payload);
+        await ticketStore.postTicketPurchase(payload);
+    }
 
-    await ticketStore.postTicketReservation(reservationData);
-
-
-    console.log("parkingSpots: ", typeof (parkingSpots), parkingSpots);
-
+    else if(!userStore.userID && guestEmail.value) {
+        const payload = {
+            location_id: ticketStore.location.id,
+            movie_id: ticketStore.movie.id,
+            screening_id: ticketStore.time.id,
+            parkingspot: parkingSpots.join(','),
+            guest_email: guestEmail.value
+        };
+        console.log("Purchase guest payload: ", payload);
+        await ticketStore.postTicketPurchase(payload);
+    }
 
     ticketStore.$reset();
     router.push("/ThankYouPage");
 };
 
+
+
+
+
+const isLoading = ref(false);
 onMounted(async () => {
+    isLoading.value = true;
     await userStore.getUser();
+    isLoading.value = false;
 })
 </script>
 
 <template>
     <BaseLayout>
-        <Stepper :currentStep="4" />
-        <div class="flex justify-center mt-20 mx-4">
-            <div
-                class=" via-white pink-600 shadow-xl border-[3px] border-dashed border-slate-500/75 rounded-3xl w-full max-w-lg relative 
-                py-8 px-6 ticket overflow-hidden
-                md: pb-4">
-                <!-- Jegy perforáció -->
-                <div
-                    class="absolute left-0 top-16 h-32 w-6 bg-white border-r-4 border-dashed border-indigo-300 rounded-r-2xl -z-10">
-                </div>
-                <div
-                    class="absolute right-0 bottom-16 h-32 w-6 bg-white border-l-4 border-dashed border-pink-300 rounded-l-2xl -z-10">
-                </div>
-                <!-- Mozi neve -->
-                <div class="text-xl text-black/75 font-bold mb-1 uppercase text-center">Drive-in mozi foglalás
-                    megerősítése</div>
-                <div class="text-2xl lg:text-3xl mt-4 lg:underline underline-offset-4 font-bold text-pink-600 mb-2 lg:text-center">{{ locationName }}</div>
-                <!-- Film -->
-                <div class="flex items-center gap-4 py-5  border-pink-100 flex-col md:flex-row w-full">
-                    <div>
-                    <img v-if="moviePoster" :src="`../src/assets/img/${moviePoster}`"
-                        class="w-full h-fit object-cover rounded-lg border border-gray-200 shadow-md" />
+        <div v-if="isLoading">
+            <BaseSpinner />
+        </div>
+        <div v-else>
+            <Stepper :currentStep="4" />
+            <div class="flex justify-center mt-20 mx-4">
+                <div class=" via-white pink-600 shadow-xl border-[3px] border-dashed border-slate-500/75 rounded-3xl w-full max-w-lg relative 
+                    py-8 px-6 ticket overflow-hidden
+                    md: pb-4">
+                    <!-- Jegy perforáció -->
+                    <div
+                        class="absolute left-0 top-16 h-32 w-6 bg-white border-r-4 border-dashed border-indigo-300 rounded-r-2xl -z-10">
                     </div>
-                    <div>
-                        <div class="text-sm underline underline-offset-4 text-pink-900 font-bold uppercase opacity-70 tracking-wider mb-2">
-                            Kiválasztott film:
+                    <div
+                        class="absolute right-0 bottom-16 h-32 w-6 bg-white border-l-4 border-dashed border-pink-300 rounded-l-2xl -z-10">
+                    </div>
+                    <!-- Mozi neve -->
+                    <div class="text-xl text-black/75 font-bold mb-1 uppercase text-center">Drive-in mozi foglalás
+                        megerősítése</div>
+                    <div
+                        class="text-2xl lg:text-3xl mt-4 lg:underline underline-offset-4 font-bold text-pink-600 mb-2 lg:text-center">
+                        {{ locationName }}</div>
+                    <!-- Film -->
+                    <div class="flex items-center gap-4 py-5  border-pink-100 flex-col md:flex-row w-full">
+                        <div>
+                            <img v-if="moviePoster" :src="`../src/assets/img/${moviePoster}`"
+                                class="w-full h-fit object-cover rounded-lg border border-gray-200 shadow-md" />
+                        </div>
+                        <div>
+                            <div
+                                class="text-sm underline underline-offset-4 text-pink-900 font-bold uppercase opacity-70 tracking-wider mb-2">
+                                Kiválasztott film:
                             </div>
-                        <div class="mx-auto text-lg font-bold text-gray-700">{{ movieTitle }}</div>
-                        <div class="mt-2 italic text-gray-500">{{ movieDesc }}</div>
+                            <div class="mx-auto text-lg font-bold text-gray-700">{{ movieTitle }}</div>
+                            <div class="mt-2 italic text-gray-500">{{ movieDesc }}</div>
+                        </div>
                     </div>
-                </div>
-                <!-- Időpont -->
-                <div class="flex flex-col gap-2">
-                    <div class="underline underline-offset-4 text-pink-900 font-bold uppercase opacity-70 text-sm lg:text-base">Vetítés időpontja</div>
-                    <div class="text-lg font-bold tracking-wider text-gray-700"
-                        style="font-family: 'Nunito','Arial','Times New Roman', Times, serif;">
-                        {{ formattedTime }}
+    
+                    <!-- Foglalási, vagy vásárlási select rész -->
+                    <div v-if="!userStore.userID" class="flex flex-col gap-2">
+                        <FormKit type="email" name="email" v-model="guestEmail" label="Email cím" input-class="p-2 border-pink-600 border-2 rounded-lg"/>
                     </div>
-                </div>
-                <!-- Parkolóhely(ek) -->
-                <div class="flex flex-col gap-2 mt-6 border-y-4 border-dashed border-slate-500 py-4 md:w-4/5">
-                    <div class="text-pink-800 font-extrabold lg:font-bold tracking-wider uppercase opacity-70 text-sm lg:text-base">Foglalásod a következő hely(ek)re
-                        szól:</div>
-                    <div class="flex gap-2 flex-wrap mt-1">
-                        <span v-for="(place, i) in parkings" :key="i"
-                            class="inline-flex items-center gap-1 px-2 py-1 md:px-4 md:py-2 rounded-full border font-bold bg-gradient-to-br 
-                            from-pink-100 to-pink-300 border-pink-300 text-pink-900 shadow-sm tracking-wide">
-                            <span class="material-icons text-lg text-black/75">{{ place }}</span> 
-                        </span>
+                    <div v-if="userStore.userID" class="flex flex-col gap-2">
+                        <label class="flex items-center gap-2">
+                            <input type="radio" value="reservation" v-model="bookingType" />
+                            Előfoglalom
+                        </label>
+                        <label class="flex items-center gap-2">
+                            <input type="radio" value="purchase" v-model="bookingType" />
+                            Megveszem
+                        </label>
                     </div>
-                </div>
-                <div
-                    class="italic mt-6 md:mt-2 pt-4 text-center text-lg font-medium text-gray-700">
-                    Jó szórakozást kívánunk a Cinema Road csapata!
+    
+                    <!-- Időpont -->
+                    <div class="flex flex-col gap-2">
+                        <div
+                            class="underline underline-offset-4 text-pink-900 font-bold uppercase opacity-70 text-sm lg:text-base">
+                            Vetítés időpontja</div>
+                        <div class="text-lg font-bold tracking-wider text-gray-700"
+                            style="font-family: 'Nunito','Arial','Times New Roman', Times, serif;">
+                            {{ formattedTime }}
+                        </div>
+                    </div>
+                    <!-- Parkolóhely(ek) -->
+                    <div class="flex flex-col gap-2 mt-6 border-y-4 border-dashed border-slate-500 py-4 md:w-4/5">
+                        <div
+                            class="text-pink-800 font-extrabold lg:font-bold tracking-wider uppercase opacity-70 text-sm lg:text-base">
+                            Foglalásod a következő hely(ek)re
+                            szól:</div>
+                        <div class="flex gap-2 flex-wrap mt-1">
+                            <span v-for="(place, i) in parkings" :key="i" class="inline-flex items-center gap-1 px-2 py-1 md:px-4 md:py-2 rounded-full border font-bold bg-gradient-to-br 
+                                from-pink-100 to-pink-300 border-pink-300 text-pink-900 shadow-sm tracking-wide">
+                                <span class="material-icons text-lg text-black/75">{{ place }}</span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="italic mt-6 md:mt-2 pt-4 text-center text-lg font-medium text-gray-700">
+                        Jó szórakozást kívánunk a Cinema Road csapata!
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="flex justify-center my-10">
-            <button class="text-lg px-10 py-3 bg-pink-700 border border-white/55 shadow-md shadow-pink-400/45 text-white uppercase font-bold rounded-xl hover:bg-pink-600 transition"
-                @click="handleConfirmation">
-                Foglalás véglegesítése
-            </button>
+            <div class="flex justify-center my-10">
+                <button
+                    class="text-lg px-10 py-3 bg-pink-700 border border-white/55 shadow-md shadow-pink-400/45 text-white uppercase font-bold rounded-xl hover:bg-pink-600 transition"
+                    @click="handleConfirmation">
+                    Foglalás véglegesítése
+                </button>
+            </div>
         </div>
     </BaseLayout>
 </template>

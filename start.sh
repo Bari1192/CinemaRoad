72d2db9d613ce9468e986ff4887bc7a5d2a536d7
bari1192 <barabas.m4rk@gmail.com>
2025-08-26 14:30:03 +0000

    'normal', 'demo' and 'dev' functions has been implemented to the start.sh file with different functions.

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
-DEMO=0
+RUN_CASE=0
 for arg in "$@"; do
   case "$arg" in
-    --demo|–demo|—demo) DEMO=1 ;;
+    --demo|–demo|—demo) RUN_CASE=1 ;;
+    --dev) RUN_CASE=2 ;;
   esac
 done
 
@@ -23,11 +24,16 @@ docker compose up -d
 
 docker compose exec backend composer install
 
-if [ "$DEMO" -eq 1 ]; then
-  echo "DEMO üzemmód aktiválva - Seeder - Tesztek - Linkek"
+if [ "$RUN_CASE" -eq 1 ]; then
+  echo "Demo üzemmód aktiválva - Seeder - Tesztek - Linkek"
   docker compose exec backend php artisan migrate:fresh --seed
   docker compose exec backend php artisan storage:link
   docker compose exec backend php artisan test
+fi
+if [ "$RUN_CASE" -eq 2 ]; then
+  echo "DEV üzemmód aktiválva - Seeder - Linkek"
+  docker compose exec backend php artisan migrate:fresh --seed
+  docker compose exec backend php artisan storage:link
 fi
 
 if [ -z "${APP_KEY}" ]; then

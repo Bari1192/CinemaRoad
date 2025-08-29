f791a8e4015a594fe8631818195513352bb48eb9
NemethTamas1 <nemethtamas029@gmail.com>
2025-08-29 11:33:58 +0000

    Now there is no record in 'purchases' where both user_id and guest_email is null

--- backend/database/factories/PurchaseFactory.php
+++ backend/database/factories/PurchaseFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Movie;
 use App\Models\Screening;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 
@@ -12,6 +13,8 @@ class PurchaseFactory extends Factory
 
     public function definition()
     {
+        $isGuest = $this->faker->boolean(50);
+
         $screening = Screening::inRandomOrder()->first();
         $movie = $screening ? $screening->movie : Movie::inRandomOrder()->first();
         return [
@@ -19,8 +22,10 @@ class PurchaseFactory extends Factory
             'movie_id' => $screening->movie_id,
             'location_id' => $screening->drive_in_cinema_id,
             "ticket_code" => bin2hex(random_bytes(12)),
-            'guest_email' => $this->faker->optional()->safeEmail,
             'parkingspot' => chr(mt_rand(65, 69)) . mt_rand(1, 8),
+            
+            'guest_email' => $isGuest ? $this->faker->safeEmail() : null,
+            'user_id' => $isGuest ? null : User::inRandomOrder()->first()->id,
         ];
     }
 }

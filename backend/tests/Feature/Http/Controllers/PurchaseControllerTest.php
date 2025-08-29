2dbbed8cc999d3ba2f00b801c8293e4f5302faa0
NemethTamas1 <nemethtamas029@gmail.com>
2025-08-29 11:29:43 +0000

    PurchaseController tests added

--- backend/tests/Feature/Http/Controllers/PurchaseControllerTest.php
+++ backend/tests/Feature/Http/Controllers/PurchaseControllerTest.php
@@ -79,6 +79,27 @@ class PurchaseControllerTest extends TestCase
         ]);
     }
 
+    public function test_user_can_not_purchase_an_already_taken_parkingspot()
+    {
+        $purchase = $this->postJson("/api/purchases",[
+            "location_id" => 1,
+            "movie_id" => 1,
+            "screening_id" => 1,
+            "guest_email" => "guestemail@gmail.com",
+            "parkingspot" => "A1",
+        ]);
+
+        $userPurchaseResponse = $this->postJson("/api/purchases", [
+            "location_id" => 1,
+            "movie_id" => 1,
+            "screening_id" => 1,
+            "user_id" => 1,
+            "parkingspot" => "A1",
+        ]);
+
+        $userPurchaseResponse->assertStatus(422);
+    }
+
     // G U E S T
     public function test_guest_can_post_purchase()
     {
@@ -141,5 +162,26 @@ class PurchaseControllerTest extends TestCase
                 "updated_at"
             ]
         ]);
+    }
+
+    public function test_guest_can_not_purchase_an_already_taken_parkingspot()
+    {
+        $reservation = $this->postJson("/api/reservations", [
+            "user_id" => 1,
+            "screening_id" => 1,
+            "reserved_at" => now()->toDateString(),
+            "location_id" => 1,
+            "parkingspot" => "A1",
+        ]);
+
+        $guestPurchase = $this->postJson("/api/purchases",[
+            "location_id" => 1,
+            "movie_id" => 1,
+            "screening_id" => 1,
+            "guest_email" => "guestemail@gmail.com",
+            "parkingspot" => "A1",
+        ]);
+
+        $guestPurchase->assertStatus(422);
     }
 }
